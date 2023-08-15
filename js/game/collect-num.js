2e5817b3ca9c915d7d7222d8b8ed1685d3e043e1
tsubasa-alife <tsubasa.robotics@gmail.com>
2023-08-15 11:06:44 +0900

    chokudaiサーチも実装

--- js/game/collect-num.js
+++ js/game/collect-num.js
@@ -3,6 +3,7 @@ class CollectNum
 	state;
 	searchMethod;
 	isGameEnd;
+	methodElement;
 	turnElement;
 	scoreElement;
 
@@ -14,6 +15,7 @@ class CollectNum
 		this.imgCharacter.className = "obj";
 		this.searchMethod = "random";
 		this.isGameEnd = false;
+		this.methodElement = document.getElementById("method");
 		this.turnElement = document.getElementById("turn");
 		this.scoreElement = document.getElementById("score");
 	}
@@ -88,6 +90,60 @@ class CollectNum
 		return bestState.firstAction;
 	}
 
+	// Chokudaiサーチ
+	chokudaiSearchAction(state, beamWidth, beamDepth, beamNumber) {
+		let beam = [];
+		for (let d = 0; d < beamDepth + 1; d++) {
+			beam[d] = [];
+		}
+		beam[0].push(state.clone());
+		for (let cnt = 0; cnt < beamNumber; cnt++) {
+			for (let d = 0; d < beamDepth; d++) {
+				let nowBeam = beam[d];
+				let nextBeam = beam[d+1];
+				for (let w = 0; w < beamWidth; w++) {
+					if (nowBeam.length == 0) {
+						console.log("nowBeamが空になったのでnextBeamを探索開始");
+						break;
+					}
+					let bestIndex = this.getBestIndex(nowBeam);
+					let nowState = nowBeam[bestIndex];
+					if (nowState.isDone()) {
+						console.log("制限ターンに達したので探索終了");
+						break;
+					}
+					nowBeam.splice(bestIndex, 1);
+
+					let actions = nowState.legalActions();
+					for (let action of actions) {
+						let nextState = nowState.clone();
+						nextState.advance(action);
+						nextState.evaluate();
+						if (d == 0) {
+							nextState.firstAction = action;
+						}
+						nextBeam.push(nextState);
+					}
+				}
+
+			}
+		}
+
+		for (let d = beamDepth; d >= 0; d--) {
+			let nowBeam = beam[d];
+			if (nowBeam.length > 0) {
+				let bestIndex = this.getBestIndex(nowBeam);
+				let bestState = nowBeam[bestIndex];
+				console.log("探索終了");
+				console.log(bestState.firstAction);
+				return bestState.firstAction;
+			}
+		}
+
+		console.log("探索終了");
+		return -1;
+	}
+
 	init() {
 		this.state = new MazeState();
 		this.state.init(8,8);
@@ -108,11 +164,13 @@ class CollectNum
 		document.getElementById("random").addEventListener("click", this.doRandomSearch.bind(this));
 		document.getElementById("greedy").addEventListener("click", this.doGreedySearch.bind(this));
 		document.getElementById("beam").addEventListener("click", this.doBeamSearch.bind(this));
+		document.getElementById("chokudai").addEventListener("click", this.doChokudaiSearch.bind(this));
 		document.getElementById("reset").addEventListener("click", this.reset.bind(this));
 	}
 
 	reset() {
 		this.deleteCharacter(this.state.charaPos.x, this.state.charaPos.y);
+		this.resetCells();
 		this.state = new MazeState();
 		this.state.init(8,8);
 		for (let i = 0; i < this.state.H; i++) {
@@ -134,9 +192,22 @@ class CollectNum
 		this.scoreElement.textContent = "得点:";
 	}
 
+	resetCells()
+	{
+		for (let i = 0; i < this.board.height; i++)
+		{
+			for (let j = 0; j < this.board.width; j++)
+			{
+				let cell = document.getElementById("cell-" + i + "-" + j);
+				cell.style.backgroundColor = "green";
+			}
+		}
+	}
+
 	doRandomSearch() {
 		if (!this.isGameEnd) {
 			this.searchMethod = "random";
+			this.methodElement.textContent = "探索アルゴリズム: ランダム";
 			console.log("ランダム法による探索を開始します");
 			this.playGame();		
 		}
@@ -145,6 +216,7 @@ class CollectNum
 	doGreedySearch() {
 		if (!this.isGameEnd) {
 			this.searchMethod = "greedy";
+			this.methodElement.textContent = "探索アルゴリズム: 貪欲法";
 			console.log("貪欲法による探索を開始します");
 			this.playGame();		
 		}
@@ -153,7 +225,17 @@ class CollectNum
 	doBeamSearch() {
 		if (!this.isGameEnd) {
 			this.searchMethod = "beam";
+			this.methodElement.textContent = "探索アルゴリズム: ビームサーチ";
 			console.log("ビームサーチによる探索を開始します");
+			this.playGame();
+		}
+	}
+
+	doChokudaiSearch() {
+		if (!this.isGameEnd) {
+			this.searchMethod = "chokudai";
+			this.methodElement.textContent = "探索アルゴリズム: Chokudaiサーチ";
+			console.log("Chokudaiサーチによる探索を開始します");
 			this.playGame();
 		}
 	}
@@ -171,12 +253,16 @@ class CollectNum
 			else if(this.searchMethod == "beam") {
 				action = this.beamSearchAction(this.state, 2, 7);
 			}
+			else if(this.searchMethod == "chokudai") {
+				action = this.chokudaiSearchAction(this.state, 1, 7, 2);
+			}
 			this.state.advance(action);
 			await this.sleep(2000);
 			console.log(this.state.toString());
 			let newPos = {x: this.state.charaPos.x, y: this.state.charaPos.y};
 			this.deleteCharacter(oldPos.x, oldPos.y);
 			this.drawCharacter(newPos.x, newPos.y, this.imgCharacter);
+			document.getElementById("cell-" + newPos.y + "-" + newPos.x).style.backgroundColor = "yellow";
 			this.showTurn(this.state.turn);
 			this.showScore(this.state.gameScore);
 		}
